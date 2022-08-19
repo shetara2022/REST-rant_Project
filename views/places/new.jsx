06fdabcc3eafb94ee65545dd838b8ea0a67bf0ff
shetara2022 <smithshetarall@gmail.com>
2022-08-19 12:38:50 -0400

    added and rendered new view

---
+++ views/places/new.jsx
@@ -0,0 +1,18 @@
+//Import React and Def component from default.jsx
+const React = require('react')
+const Def = require('../default')
+
+function new_form() { 
+    return (
+        <Def>
+            <html>
+                <main>
+                    <h1>New Place</h1>
+                </main>
+            </html>
+        </Def>
+    )
+}
+
+
+module.exports = new_form
