a08669ed1aec3711da2bec8dfc02e9901a163fe3
shetara2022 <smithshetarall@gmail.com>
2022-08-19 15:34:46 -0400

    new places can be added cd ..

--- controllers/places.js
+++ controllers/places.js
@@ -3,20 +3,6 @@ const places = require('../models/places.js')
 
 
 router.get('/', (req, res) => {
-    // let places = [{
-    //     name: 'Twinkles BBQ',
-    //     city: 'Raleigh',
-    //     state: 'NC',
-    //     cuisines: 'American BBQ',
-    //     pic: '/images/bbq.jpeg'      //exported to places.js
-    // }, {
-    //     name: 'Je suis Yum',
-    //     city: 'Durham',
-    //     state: 'NC',
-    //     cuisines: 'French',
-    //     pic: '/images/french.jpeg'
-    // }]
-
     res.render('places/index', { places })
 })
 
@@ -26,18 +12,18 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
     console.log(req.body)
-    // if (!req.body.pic) {
-    //     // Default image if one is not provided
-    //     req.body.pic = 'http://placekitten.com/400/400'
-    // }
-    // if (!req.body.city) {
-    //     req.body.city = 'Anytown'
-    // }
-    // if (!req.body.state) {
-    //     req.body.state = 'USA'
-    // }
-    // places.push(req.body)
-    res.send('/places')
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
 
 
