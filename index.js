fd4b5d5120c52ee4966be309ea56ea7c1500a17c
shetara2022 <smithshetarall@gmail.com>
2022-08-21 10:48:55 -0400

    delete functionality implemented

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 
 //Initialize express app variable 
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 
 //Defines the JSX view engine. These are the Express Settings 
@@ -11,6 +12,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 //Controllers & Routes
 app.use('/places', require('./controllers/places'))
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,24 @@
 const React = require('react')
 const Def = require('../default')
 
-function show() {
+function show(data) {
     return (
         <Def>
             <main>
-                <h1>Show Page</h1>
+                <h1>{data.place.name}</h1>
+                <h3>Rating</h3>
+                <h3>Description</h3>
+                <h4>Add a Comment</h4>
+                <p>No comments yet!</p>
+                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                Edit
+                </a>  
+                <form method="POST" action={`/places/${data.id}?_method=DELETE` }> 
+                <button type="submit" className="btn btn-danger">
+                Delete
+                </button>
+                </form>     
+
             </main>
     </Def>
 )
