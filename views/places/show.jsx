41c6d534b73a8a3d77fcfd6b12ed4f3dd0d27dc8
shetara2022 <smithshetarall@gmail.com>
2022-08-21 10:23:42 -0400

    show route connected

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,7 +2,13 @@ const React = require('react')
 const Def = require('../default')
 
 function show() {
-
+    return (
+        <Def>
+            <main>
+                <h1>Show Page</h1>
+            </main>
+    </Def>
+)
 
 }
 
