e2c5d85083a8b4ad40ff5c522d9b7a281afaf8d3
shetara2022 <smithshetarall@gmail.com>
2022-08-24 16:57:15 -0400

    corrected edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -1,15 +1,17 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-
+//Index
 router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
+//New
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+//Create
 router.post('/', (req, res) => {
     // console.log(req.body)
     if (!req.body.pic) {
@@ -36,7 +38,7 @@ router.get('/:id/edit', (req, res) => {
         res.render('error404')
     }
     else {
-        res.render('places/edit', { place: places[id] })
+        res.render('places/edit', { place: places[id], id })
     }
 })
 
@@ -55,7 +57,53 @@ router.get('/:id', (req, res) => {
 })
 
 
-//delete route
+//Update(PUT)route
+router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+
+        // Save the new data into places[id]
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
+    }
+})
+
+// router.put('/:id', (req, res) => {
+//     let id = Number(req.params.id)
+//     if (isNaN(id)) {
+//         res.render('error404')
+//     }
+//     else if (!places[id]) {
+//         res.render('error404')
+//     }
+//     // else {
+//     //     res.render('error404')
+//     // }
+//     //Save the new data into places[id]
+//     places[id] = req.body
+//     res.redirect(`/places/${id}`)
+// })
+
+
+
+//Delete route
 router.delete('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,34 +1,44 @@
 const React = require('react')
-const Def = require('../default')
+const Def = require('../default.jsx')
 
 function edit_form(data) {
     return (
         <Def>
             <main>
                 <h1>Edit Place</h1>
-                <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+                <form method="POST" action={`/places/${data.id}?_method=PUT`}>
                     <div className="row">
-                        <div className="form-group">
+                        <div className="form-group col-sm-6">
                             <label htmlFor="name">Place Name</label>
                         <input className="form-control"
                             id="name"
                             name="name"
-                            value= {data.place.name}
+                            defaultValue= {data.place.name}
                             required />
                         </div>
-                        <div className="form-group">
+                        <div className="form-group col-sm-6">
                             <label htmlFor="pic">Place Image</label>
-                            <input className="form-control" type ="url" id="pic" name ="pic" />
+                            <input className="form-control"
+                                // type="url"
+                                id="pic"
+                                name="pic"
+                                defaultValue= {data.place.pic}/>
                         </div>
-                        <div className="form-group">
+                        <div className="form-group col-sm-6">
                             <label htmlFor="city">City</label>
-                            <input className="form-control" id ="city" name="city" />
+                            <input className="form-control"
+                                id="city"
+                                name="city"
+                                defaultValue= {data.place.city}/>
                         </div>
-                        <div className="form-group">
+                        <div className="form-group col-sm-6">
                             <label htmlFor="state">State</label>
-                            <input className="form-control" id ="state" name="state" />
+                            <input className="form-control"
+                                id="state"
+                                name="state"
+                                defaultValue= {data.place.state}/>
                         </div>
-                        <div className="form-group">
+                        <div className="form-group col-sm-6">
                             <label htmlFor="cuisines">Cuisines</label>
                             <input className="form-control" id ="cuisines" name="cuisines" required/>
                         </div>
