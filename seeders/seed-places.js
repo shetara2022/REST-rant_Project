a2f7f66deda9deb7957773faebd83755f0a7a37f
shetara2022 <smithshetarall@gmail.com>
2022-08-29 17:06:26 -0400

    added validation error message

--- seeders/seed-places.js
+++ seeders/seed-places.js
@@ -5,7 +5,7 @@ db.Place.create([{
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
-    pic: '/images/thai image.jpeg',
+    pic: 'http://placekitten.com/g/200/300',
 }, {
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,9 +5,21 @@ function show(data) {
     return (
         <Def>
             <main>
+                <div className="row">
+                    <div className='col-sm-6'>
+                        <img src={data.place.pic} alt={data.place.name} />
+                        <h3>
+                            Located in {data.place.city}, {data.city.state}
+                        </h3> 
+                    </div>
+                    <div className='col-sm-6'>
+
+                   
                 <h1>{data.place.name}</h1>
-                <h3>Rating</h3>
-                <h3>Description</h3>
+                <h2>Rating</h2>
+                <h2>Description</h2>
+                <h3>{data.place.showEstablished()}</h3>
+                <h4>Serving {data.place.cuisines}</h4>
                 <h4>Add a Comment</h4>
                 <p>No comments yet!</p>
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
@@ -17,7 +29,9 @@ function show(data) {
                 <button type="submit" className="btn btn-danger">
                 Delete
                 </button>
-                </form>     
+                        </form>
+                 </div>
+                </div>        
 
             </main>
     </Def>
