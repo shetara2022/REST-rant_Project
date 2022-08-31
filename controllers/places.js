2441a242ffd925ed2ea9101f951c43d24b4009d9
shetara2022 <smithshetarall@gmail.com>
2022-08-30 21:17:50 -0400

    successfully loaded comments

--- controllers/places.js
+++ controllers/places.js
@@ -48,7 +48,9 @@ router.get('/new', (req, res) => {
 //show route 
 router.get('/:id', (req, res) => {
     db.Place.findById(req.params.id)
+        .populate('comments')
         .then(place => {
+            console.log(place.comments)
             res.render('places/show', { place })
         })
         .catch(err => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show(data) {
+    let comments = (
+        <h3 className='inactive'>
+            No comments yet!
+        </h3>
+    )
+        if (data.place.comments.length) {
+          comments = data.place.comments.map(c => {
+            return (
+              <div className="border">
+                <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+                <h4>{c.content}</h4>
+                <h3>
+                  <stong>- {c.author}</stong>
+                </h3>
+                <h4>Rating: {c.stars}</h4>
+              </div>
+            )
+          })
+        }
     return (
         <Def>
             <main>
@@ -9,7 +28,7 @@ function show(data) {
                     <div className='col-sm-6'>
                         <img src={data.place.pic} alt={data.place.name} />
                         <h3>
-                            Located in {data.place.city}, {data.city.state}
+                            Located in {data.place.city}, {data.place.state}
                         </h3> 
                     </div>
                     <div className='col-sm-6'>
@@ -21,7 +40,7 @@ function show(data) {
                 <h3>{data.place.showEstablished()}</h3>
                 <h4>Serving {data.place.cuisines}</h4>
                 <h4>Add a Comment</h4>
-                <p>No comments yet!</p>
+                {comments}
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                 Edit
                 </a>  
