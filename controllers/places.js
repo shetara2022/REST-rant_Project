c6f4c76cd283533c97f13c5b93e3540438af303e
shetara2022 <smithshetarall@gmail.com>
2022-09-10 12:16:50 -0400

    deleted unneeded code

--- controllers/places.js
+++ controllers/places.js
@@ -114,29 +114,6 @@ router.post('/:id/comment', (req, res) => {
         })
 })
 
-// router.post('/:id/comment', (req, res) => {
-//     console.log(req.body)
-//     req.body.rant = req.body.rant ? true : false
-//     db.Place.findById(req.params.id)
-//     .then(place => {
-//         db.Comment.create(req.body)
-//         .then(comment => {
-//             place.comments.push(comment.id)
-//             place.save()
-//             .then(() => {
-//                 res.redirect(`/places/${req.params.id}`)
-//             })
-//         })
-//         .catch(err => {
-//             res.render('error404')
-//         })
-//     })
-//     .catch(err => {
-//         res.render('error404')
-//     })
-// })
-
-
     //Delete route
     router.delete('/:id', (req, res) => {
         db.Place.findByIdAndDelete(req.params.id)
